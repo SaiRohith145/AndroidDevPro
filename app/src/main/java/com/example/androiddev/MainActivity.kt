fd84e6e5a82532a23ddf3f4c9a507318318d24fe
SaiRohith145 <m.sairohith145@gmail.com>
2023-07-29 16:23:25 +0530

    ImageView in Android

--- app/src/main/java/com/example/androiddev/MainActivity.kt
+++ app/src/main/java/com/example/androiddev/MainActivity.kt
@@ -3,21 +3,29 @@ package com.example.androiddev
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
 import com.example.androiddev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var isVisible:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnAdd.setOnClickListener {
-            val firstName = binding.etFirstNumber.text.toString()
-            val lastName = binding.etSecondNumber.text.toString()
-            var result: Int = firstName.toInt()+lastName.toInt()
-            binding.tvResult.text = result.toString()
+            if (isVisible){
+                isVisible =false
+                binding.ivWallp.isVisible = false
+            } else {
+                isVisible= true
+                binding.ivWallp.setImageResource(R.drawable.wallpaper)
+                binding.ivWallp.isVisible = true
+            }
+
         }
     }
 }
